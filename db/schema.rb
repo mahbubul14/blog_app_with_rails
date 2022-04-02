3337dd10eae1919b9550d4ec340b0a77109e76ce
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-03 01:49:04 +0600

    Update with optional suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -16,19 +16,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_140209) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.bigint "author_id"
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
@@ -38,10 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_140209) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "author_id"
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,13 +49,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_01_140209) do
     t.text "bio"
     t.string "photo"
     t.integer "posts_counter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "posts", "users"
 end
