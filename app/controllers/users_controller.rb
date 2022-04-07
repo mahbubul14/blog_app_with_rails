69f7b0d7e4b29218d06204622e0c4c376b9f7ea2
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-07 21:32:50 +0600

    Fix merge issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,9 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!
-
   def index
-    user_signed_in?
-    current_user
-    user_session
-    @user = User.all
-    @posts = Post.all
+    @users = User.all
   end
 
   def show
-    @likes = Like.all.includes(:post_id)
-    @comments = Comment.all.includes(:post_id).order(created_at: :desc)
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by_id(params[:id])
   end
 end
