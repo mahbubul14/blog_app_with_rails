2830a527d9d3d4482370c4fe452ed99a47948095
Claude Mucyo Ntacyonungutse <mucyosofa@gmail.com>
2022-04-07 17:33:06 +0200

    Update user_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
+
   def index
-    @users = User.all
+    user_signed_in?
+    current_user
+    user_session
+    @user = User.all
+    @posts = Post.all
   end
 
   def show
-    @user = User.find_by_id(params[:id])
+    @likes = Like.all.includes(:post_id)
+    @comments = Comment.all.includes(:post_id).order(created_at: :desc)
+    @user = User.find_by(id: params[:id])
   end
 end
