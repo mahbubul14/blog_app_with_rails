170ecc5d4d0edef683c0191b5af0acaa5fe0450a
mahbubul14 <mahbub21alam2017@gmail.com>
2022-04-08 23:47:42 +0600

    Fix rubocop issues

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe 'Login page', js: true, type: :feature do
   describe 'Login Page requirements' do
     it 'has username and password inputs, and Log in button' do
